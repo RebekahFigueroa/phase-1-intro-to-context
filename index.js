894f8ebc3cc69daf37f788b0547fd93aff6b5e49
Rebekah <figueroarebekah@gmail.com>
2023-01-16 19:24:04 -0600

    commit tree ac013472131f102af15e29c649fe679b8258612c

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Your code here
+function createEmployeeRecord(array) {
+	const [firstName, familyName, title, payPerHour] = array;
+	return {
+		firstName: firstName,
+		familyName: familyName,
+		title: title,
+		payPerHour: payPerHour,
+		timeInEvents: [],
+		timeOutEvents: [],
+	};
+}
+
+function createEmployeeRecords(arrays) {
+	return arrays.map((array) => createEmployeeRecord(array));
+}
+
+function createTimeInEvent(objectEmployeeRecord, stringDateStamp) {
+	const timeSplit = stringDateStamp.split(" ");
+	objectEmployeeRecord.timeInEvents.push({
+		type: "TimeIn",
+		hour: parseInt(timeSplit[1], 10),
+		date: timeSplit[0],
+	});
+	return objectEmployeeRecord;
+}
+
+function createTimeOutEvent(objectEmployeeRecord, stringDateStamp) {
+	const timeSplit = stringDateStamp.split(" ");
+	objectEmployeeRecord.timeOutEvents.push({
+		type: "TimeOut",
+		hour: parseInt(timeSplit[1], 10),
+		date: timeSplit[0],
+	});
+	return objectEmployeeRecord;
+}
+
+function hoursWorkedOnDate(objectEmployeeRecord, stringDate) {
+	const timeInEvent = objectEmployeeRecord.timeInEvents.find(
+		(event) => event.date === stringDate
+	);
+	const timeOutEvent = objectEmployeeRecord.timeOutEvents.find(
+		(event) => event.date === stringDate
+	);
+	return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(objectEmployeeRecord, stringDate) {
+	const hours = hoursWorkedOnDate(objectEmployeeRecord, stringDate);
+	return hours * objectEmployeeRecord.payPerHour;
+}
+
+function allWagesFor(objectEmployeeRecord) {
+	const dates = objectEmployeeRecord.timeInEvents.map((event) => event.date);
+	return dates.reduce(
+		(total, date) => total + wagesEarnedOnDate(objectEmployeeRecord, date),
+		0
+	);
+}
+
+function calculatePayroll(arrayEmployeeRecords) {
+	return arrayEmployeeRecords.reduce(
+		(total, employeeRecord) => total + allWagesFor(employeeRecord),
+		0
+	);
+}
